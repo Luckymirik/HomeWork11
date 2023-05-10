ae3614e6763bee213f101b0311a4cb347c752e78
LuckyMirik <Luckymirik@mail.ru>
2023-05-10 23:24:45 +0300

    Исправил ДЗ

--- src/Main.java
+++ src/Main.java
@@ -6,48 +6,50 @@ public class Main {
         task2();
         task3();
     }
-    public static void checkYear(int year){
+
+    public static void checkYear(int year) {
 
 
-        if ((year%4==0&&year%100!=0)||year%400==0)
-        {
-            System.out.println(year+" год является високосным");
-        }else{
-            System.out.println(year+" год не является високосным");
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
         }
 
     }
-    public static void findVersion(int clientOS,int clientDeviceYear){
 
-        if (clientOS==0) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-            if (clientDeviceYear < 2015)
+    public static void findVersion(int clientOS, int clientDeviceYear) {
+
+        if (clientOS == 0) {
+            if (clientDeviceYear > 2015) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
 
-        }
-        else {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        }if(clientDeviceYear < 2015){ System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            if (clientDeviceYear > 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
         }
 
     }
-    public static void daysCalculation(int deliveryDistance){
-        int deliveryTime;
-        if(deliveryDistance<=20){
-            deliveryTime=1;
-            System.out.println( "Потребуется дней: " + deliveryTime);
-        } else if (deliveryDistance<=60) {
-            deliveryTime=2;
-            System.out.println( "Потребуется дней: " +deliveryTime);
-        } else if (deliveryDistance<=100) {
-            deliveryTime=3;
-            System.out.println( "Потребуется дней: " + deliveryTime);
-        }else{
-            System.out.println( "Доставки нет. " );
+
+    public static int daysCalculation(int deliveryDistance) {
+
+        if (deliveryDistance <= 20) {
+            return 1;
+        } else if (deliveryDistance <= 60) {
+            return 2;
+        } else if (deliveryDistance <= 100) {
+            return 3;
+        } else {
+            return -1;
 
         }
     }
-
 
 
     public static void task1() {
@@ -55,23 +57,30 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int year = scanner.nextInt();
         checkYear(year);
-        
+
     }
+
     public static void task2() {
         System.out.println("Задача 2");
         Scanner scanner = new Scanner(System.in);
-        int clientOS= scanner.nextInt();
+        int clientOS = scanner.nextInt();
         int clientDeviceYear = scanner.nextInt();
 
-        findVersion(clientOS,clientDeviceYear);
+        findVersion(clientOS, clientDeviceYear);
 
 
     }
+
     public static void task3() {
         System.out.println("Задача 3");
         Scanner scanner = new Scanner(System.in);
-        int distance = scanner.nextInt();
-        daysCalculation(distance);
+        int deliveryDistance = scanner.nextInt();
+        if (daysCalculation(deliveryDistance) > 0) {
+            System.out.println("Потребуется дней: " + daysCalculation(deliveryDistance));
+        } else {
+            System.out.println("Доставка невозможна");
 
+
+        }
     }
 }
